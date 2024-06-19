3bff8c7b3b2c26e1be5b55495cfd60d5d9c6b6b9
kenneth <123408292+trulli-kenneth@users.noreply.github.com>
2024-06-19 06:24:29 -0400

    Initial Commit

---
+++ RoomsToGoAssignment/RoomsToGoAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsToGoAssignmentApp.swift
+//  RoomsToGoAssignment
+//
+//  Created by Kenneth Adams on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomsToGoAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
